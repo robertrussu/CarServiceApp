e62dad6af03cbad057a5067bdb0826b2dc1e335f
robertrussu <robertrussu04@gmail.com>
2025-04-28 23:10:37 +0300

    Service page

---
+++ Controllers/ServiceController.cs
@@ -0,0 +1,32 @@
+﻿using CarServiceApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarServiceApp.Controllers
+{
+    public class ServiceController : Controller
+    {
+        [HttpGet]
+        public IActionResult Schedule()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Schedule(ServiceRequest request)
+        {
+            if (ModelState.IsValid)
+            {
+                // For now just return a "Thank You" page or back to home
+                return RedirectToAction("ThankYou");
+            }
+
+            return View(request);
+        }
+
+        public IActionResult ThankYou()
+        {
+            return View();
+        }
+    }
+
+}
---
+++ Models/ServiceRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace CarServiceApp.Models
+{
+    public class ServiceRequest
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string VehicleModel { get; set; }
+        public string ProblemDescription { get; set; }
+    }
+
+}
