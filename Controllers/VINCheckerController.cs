82953ea36975f107c255e23d470d51b08600c945
robertrussu <robertrussu04@gmail.com>
2025-05-06 09:42:00 +0300

    start vinchecker

---
+++ Controllers/VINCheckerController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YourProjectName.Controllers
+{
+	public class VINCheckerController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Index(string vin)
+		{
+			if (string.IsNullOrWhiteSpace(vin))
+			{
+				ViewBag.Error = "Please enter a VIN.";
+				return View();
+			}
+
+			// Dummy API result (simulate API call)
+			var vehicleInfo = new
+			{
+				VIN = vin,
+				Make = "Toyota",
+				Model = "Supra",
+				Year = "1999",
+				Engine = "3.0L I6 Turbo"
+			};
+
+			return View("Result", vehicleInfo);
+		}
+	}
+}
