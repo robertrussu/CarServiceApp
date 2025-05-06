beff6981afa30c9cfaae6ff68df5d10bbc534e41
Andreea-Mojoiu <andr33a.mojoiu@gmail.com>
2025-05-06 09:48:11 +0300

    AdminController and MyAppointment

---
+++ Controllers/MyAppointments.cs
@@ -0,0 +1,20 @@
+﻿using CarServiceApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarServiceApp.Controllers
+{
+    [Authorize]
+    public class MyAppointmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var myRequests = new List<ServiceRequest>
+            {
+                new ServiceRequest { FullName = "You (Fake User)", Email = "you@example.com", VehicleModel = "Toyota Supra", ProblemDescription = "Brake check" }
+            };
+
+            return View(myRequests);
+        }
+    }
+}
